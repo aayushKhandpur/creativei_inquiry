680b8b13212bfcd05d8017b969f4f30fb812b7e3
Jitendra Jaria <jitendrajaria96@gmail.com>
2018-01-30 12:58:27 +0530

    follow up server info PR issue resolved

--- src/main/java/creativei/enums/CaseIndex.java
+++ src/main/java/creativei/enums/CaseIndex.java
@@ -3,16 +3,22 @@ package creativei.enums;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import creativei.controller.InquiryController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.*;
 
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum CaseIndex {
+
     UNLIKELY("Unlikely",1),
     INTERESTED("Intersted",2),
     LIKELY("Likely",3),
     HOT_LEAD("Hot Lead",4);
 
+    private static final Logger logger = LoggerFactory.getLogger(CaseIndex.class);
     private int index;
     private String value;
     private CaseIndex(String value,int index){
@@ -40,6 +46,7 @@ public enum CaseIndex {
             try {
                 list.add(new ObjectMapper().writeValueAsString(caseIndex));
             } catch (JsonProcessingException e) {
+                logger.error("JSON content can not be processed");
                 e.printStackTrace();
             }
         }

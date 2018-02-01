541f1ea6f9ebf3539feca6301fc9260c97406560
Jitendra Jaria <jitendrajaria96@gmail.com>
2018-02-01 18:50:04 +0530

    inquiry address updated according to schema

--- src/main/java/creativei/entity/InquiryAddress.java
+++ src/main/java/creativei/entity/InquiryAddress.java
@@ -15,27 +15,14 @@ public class InquiryAddress extends BaseEntity implements Serializable {
 
     public InquiryAddress(AddressVo addressVo){
         this.setId(addressVo.getId());
-        this.area=addressVo.getArea()==null||addressVo.getArea().isEmpty()?null:addressVo.getArea();
-        this.city=addressVo.getCity()==null||addressVo.getCity().isEmpty()?null:addressVo.getCity();
         this.pincode=addressVo.getPin()==null||addressVo.getPin().isEmpty()?null:addressVo.getPin();
         this.addressLine1=addressVo.getAddressLine1()==null||addressVo.getAddressLine1().isEmpty()?null:addressVo.getAddressLine1();
-        this.country=addressVo.getCountry()==null||addressVo.getCountry().isEmpty()?null:addressVo.getCountry();
-        this.state=addressVo.getState()==null||addressVo.getState().isEmpty()?null:addressVo.getState();
         this.location=new Locality(addressVo.getLocationId());
     }
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "Inquiry.id")
     private Inquiry inquiry;
     @Column(nullable = false)
     private String pincode;
-    @Column(nullable = false)
-    private String country="INDIA";
-    @Column(nullable = false)
-    private String state;
-    @Column(nullable = false)
-    private String city="JAIPUR";
-    @Column(nullable = false)
-    private String area;
     @Column(name = "address_line_1")
     private String addressLine1;
     @Column(name="address_line_2")
@@ -43,7 +30,7 @@ public class InquiryAddress extends BaseEntity implements Serializable {
     @Column(name = "is_current")
     private boolean isCurrent = true;
     @ManyToOne(cascade = CascadeType.MERGE,fetch = FetchType.LAZY)
-    private Locality location;
+    private Locality location;;
 
     public Locality getLocation() {
         return location;
@@ -51,44 +38,6 @@ public class InquiryAddress extends BaseEntity implements Serializable {
 
     public void setLocation(Locality location) {
         this.location = location;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getPincode() {
-        return pincode;
-    }
-
-    public void setPincode(String pincode) {
-        this.pincode = pincode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {this.city = city; }
-
-    public String getArea() {
-        return area;
-    }
-
-    public void setArea(String area) {
-        this.area = area;
     }
 
     public String getAddressLine1() {
--- src/main/java/creativei/vo/AddressVo.java
+++ src/main/java/creativei/vo/AddressVo.java
@@ -6,10 +6,7 @@ import creativei.entity.InquiryAddress;
 import creativei.entity.Locality;
 
 public class AddressVo {
-    private String country;
-    private String state;
-    private String area;
-    private String city;
+
     private String pin;
     private String addressLine1;
     private Long id;
@@ -21,38 +18,6 @@ public class AddressVo {
 
     public void setLocationId(Long locationId) {
         this.locationId = locationId;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getArea() {
-        return area;
-    }
-
-    public void setArea(String area) {
-        this.area = area;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
     }
 
     public String getPin() {

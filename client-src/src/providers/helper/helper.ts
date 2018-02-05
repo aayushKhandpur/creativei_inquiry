1152b2e26eddc759a5a452d6666d9a1e07c85e81
keshavgr8 <keshavgr8@gmail.com>
2018-02-06 02:47:03 +0530

    Added Update Inquiry for counsellor

--- client-src/src/providers/helper/helper.ts
+++ client-src/src/providers/helper/helper.ts
@@ -50,4 +50,19 @@ export class HelperProvider {
     }
   }
 
+  removeEmptyFromObject = (obj) => {
+    const o = JSON.parse(JSON.stringify(obj)); // Clone source oect.
+  
+    Object.keys(o).forEach(key => {
+      if (o[key] && typeof o[key] === 'object')
+        o[key] = this.removeEmptyFromObject(o[key]);  // Recurse.
+      else if (o[key] === undefined || o[key] === null)
+        delete o[key]; // Delete undefined and null.
+      else
+        o[key] = o[key];  // Copy value.
+    });
+  
+    return o; // Return new object.
+  };
+
 }

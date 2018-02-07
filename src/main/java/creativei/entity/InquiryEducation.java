8da9452859262402137a55a05e44864890f1213a
Jitendra Jaria <jitendrajaria96@gmail.com>
2018-02-07 12:21:18 +0530

    PR issue of updateEducatioVo resolved

--- src/main/java/creativei/entity/InquiryEducation.java
+++ src/main/java/creativei/entity/InquiryEducation.java
@@ -12,42 +12,46 @@ import java.util.Iterator;
 import java.util.List;
 
 @Entity
-@Table(name="Inquiry_Education")
+@Table(name = "Inquiry_Education")
 public class InquiryEducation extends BaseEntity implements Serializable {
 
     @ManyToOne
     private Inquiry inquiry;
-    @Column(name="education_qualificaton",nullable = false)
+    @Column(name = "education_qualificaton", nullable = false)
     private EducationQualification educationQualification;
-    @Column(name="institue_name")
+    @Column(name = "institue_name")
     private String instituteName;
     private Stream stream;
     private EducationStatus status;
     private Integer year;
-    @Column(name="aggregate_marks")
+    @Column(name = "aggregate_marks")
     private Double aggregateMarks;
-    @Column(name="mark_scheme")
+    @Column(name = "mark_scheme")
     private MarkScheme markScheme;
     private String type;
 
-    public InquiryEducation(){}
-
-    public InquiryEducation(EducationVo educationVo) {
-
-            this.setId(educationVo.getId());
-            this.educationQualification = EducationQualification.stringToEnum(educationVo.getEducationQualification());
-            this.instituteName = educationVo.getInstituteName() == null || educationVo.getInstituteName().isEmpty() ? null : educationVo.getInstituteName();
-            this.stream = Stream.stringToEnum(educationVo.getStream());
-            this.status = EducationStatus.stringToEnum(educationVo.getStatus());
-            this.year = educationVo.getYear() == null ? null : educationVo.getYear();
-            this.aggregateMarks = educationVo.getAggregateMarks() == null ? null : educationVo.getAggregateMarks();
-            this.markScheme = MarkScheme.stringToEnum(educationVo.getMarkScheme());
-            this.type = educationVo.getType() == null || educationVo.getType().isEmpty() ? null : educationVo.getType();
+    public InquiryEducation() {
     }
 
-    public String getType() {return type;}
+    public InquiryEducation(EducationVo educationVo) {
+        this.setId(educationVo.getId());
+        this.educationQualification = EducationQualification.stringToEnum(educationVo.getEducationQualification());
+        this.instituteName = educationVo.getInstituteName() == null || educationVo.getInstituteName().isEmpty() ? null : educationVo.getInstituteName();
+        this.stream = Stream.stringToEnum(educationVo.getStream());
+        this.status = EducationStatus.stringToEnum(educationVo.getStatus());
+        this.year = educationVo.getYear() == null ? null : educationVo.getYear();
+        this.aggregateMarks = educationVo.getAggregateMarks() == null ? null : educationVo.getAggregateMarks();
+        this.markScheme = MarkScheme.stringToEnum(educationVo.getMarkScheme());
+        this.type = educationVo.getType() == null || educationVo.getType().isEmpty() ? null : educationVo.getType();
+    }
 
-    public void setType(String type) { this.type = type;}
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
     public EducationQualification getEducationQualification() {
         return educationQualification;
